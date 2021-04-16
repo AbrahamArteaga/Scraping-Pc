1085825b99d16a2cd70889844865d7b298ae42b7
Francisco-Jose-Rodriguez-Rugeles <frrodriguez@unal.edu.co>
2021-04-16 14:18:39 -0500

    Pila implementada

---
+++ Estructuras/pila.py
@@ -0,0 +1,59 @@
+"""
+Modulo de la clase Pila
+aqui implementamos
+nuestra propia Pila
+"""
+
+
+class Pila:
+
+    """
+    Nombre de clase: Pila
+    Atributos: cabeza, stack
+    Metodos: obtener_cabeza, agregar
+             remover, __str__, __len__
+    """
+
+    def __init__(self):
+        self.cabeza = None
+        self.stack = []
+
+    def obtener_cabeza(self):
+
+        """
+        Esta funcion retorna
+        la cabeza de la pila,
+        mas no la elimina.
+        """
+
+        return self.cabeza
+
+    def agregar(self, elemento):
+
+        """
+        Esta funcion agrega
+        datos a nuestra pila
+        """
+
+        self.cabeza = elemento
+        self.stack.append(elemento)
+
+    def remover(self):
+
+        """
+        Remueve solamente la cabeza,
+        si la lista esta vacia
+        retorna valor Falso
+        """
+
+        if self.cabeza is None:
+            pass
+        else:
+            self.stack.pop()
+            self.cabeza = self.stack[-1]
+
+    def __str__(self):
+        return str(self.stack)
+
+    def __len__(self):
+        return len(self.stack)
