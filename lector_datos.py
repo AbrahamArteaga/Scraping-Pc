f74f3e6b696ab244349f94c712cdae5b5764df38
Francisco-Jose-Rodriguez-Rugeles <frrodriguez@unal.edu.co>
2021-04-25 20:00:42 -0500

    Error de ruta arreglado

--- lector_datos.py
+++ lector_datos.py
@@ -5,7 +5,7 @@ import os
 cwd = os.getcwd()  # Get the current working directory (cwd)
 files = os.listdir(cwd)  # Get all the files in that directory
 # print("Files in %r: %s" % (cwd, files))
-direccion = f"{cwd}\\datosA\\"
+direccion = f"{cwd}/datosA/"
 
 
 def leer(nombre):
